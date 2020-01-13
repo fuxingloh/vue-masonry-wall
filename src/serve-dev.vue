178e64591e5dcc026ee617a71613fb641e302da5
Fuxing Loh <fuxing@munch.space>
2020-01-13 18:50:55 +0800

    v0.2.0

---
+++ src/serve-dev.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import VueMasonryWall from '@/vue-masonry-wall.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VueMasonryWall
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-masonry-wall />
+  </div>
+</template>
---
+++ src/vue-masonry-wall.vue
@@ -0,0 +1,60 @@
+<script>
+export default {
+  name: 'VueMasonryWall', // vue component name
+  data() {
+    return {
+      counter: 5,
+      initCounter: 5,
+    };
+  },
+  methods: {
+    increment(arg) {
+      if (typeof arg !== 'number') this.counter += 1;
+      else this.counter += arg;
+    },
+    decrement(arg) {
+      if (typeof arg !== 'number') this.counter -= 1;
+      else this.counter -= arg;
+    },
+    reset() {
+      this.counter = this.initCounter;
+    }
+  },
+}
+</script>
+
+<template>
+  <div class="vue-masonry-wall">
+    <p>The counter is set to <b>{{ counter }}</b>.</p>
+    <button @click="increment">
+      Click +1
+    </button>
+    <button @click="decrement">
+      Click -1
+    </button>
+    <button @click="increment(5)">
+      Click +5
+    </button>
+    <button @click="decrement(5)">
+      Click -5
+    </button>
+    <button @click="reset">
+      Reset
+    </button>
+  </div>
+</template>
+
+<style scoped>
+  .vue-masonry-wall {
+    display: block;
+    width: 400px;
+    margin: 25px auto;
+    border: 1px solid #ccc;
+    background: #eaeaea;
+    text-align: center;
+    padding: 25px;
+  }
+  .vue-masonry-wall p {
+    margin: 0 0 1em;
+  }
+</style>
